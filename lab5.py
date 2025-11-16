7ebffdb8b693068fb12e61d02e63bda17415b1fb
Дарина Редкачева <darinakaroche@mail.ru>
2025-11-16 15:04:20 +0700

    Удаление дублирующегося кода

--- lab5.py
+++ lab5.py
@@ -9,6 +9,23 @@ lab5 = Blueprint('lab5', __name__)
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
+
+def db_connect():
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'darina_redkacheva_knowledge_base',
+        user = 'darina_redkacheva_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+    
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -20,28 +37,24 @@ def register():
     if not login or not password:
         return render_template('lab5/register.html', error='Заполните все поля')
 
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'darina_redkacheva_knowledge_base',
-        user = 'darina_redkacheva_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor()
+    conn, cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
-    if cur.fetchone():
+    cur.execute("SELECT * FROM users WHERE login = %s;", (login,))
+    user = cur.fetchone()
+    if user:
+        db_close(conn, cur)
         return render_template('lab5/register.html',
                                 error="Такой пользователь уже существует")
 
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);",
+                (login, password))
+
+    db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
 
-@lab5.route('/lab5/register', methods = ['GET', 'POST'])
-def register():
+@lab5.route('/lab5/login', methods = ['GET', 'POST'])
+def login():
     if request.method == 'GET':
         return render_template('lab5/login.html')
     
@@ -51,30 +64,23 @@ def register():
     if not login or not password:
         return render_template('lab5/login.html', error='Заполните все поля')
 
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'darina_redkacheva_knowledge_base',
-        user = 'darina_redkacheva_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor(cursor_factory = RealDictCursor)
+    conn, cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login = %s;", (login,))
     user = cur.fetchone()
 
     if not user:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html',
                                 error='Логин и/или пароль неверны')
     
     if user['password'] != password:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html',
                                 error='Логин и/или пароль неверны')
     
     session['login'] = login
-    cur.close()
-    conn.close()
+
+    db_close(conn, cur)
+
     return render_template('lab5/success_login.html', login=login)
