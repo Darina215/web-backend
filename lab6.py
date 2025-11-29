83db1851c6273db674403c206f5ec607b702ffd5
Редкачева Дарина <darinakaroche@mail.ru>
2025-11-29 21:59:33 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -15,6 +15,7 @@ def main():
 def api():
     data = request.json
     id = data['id']
+
     if data['method'] == 'info':
         return {
             'jsonrpc': '2.0',
@@ -48,6 +49,36 @@ def api():
                     }
 
                 office['tenant'] = login
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office is not booked'
+                        },
+                        'id': id
+                    }
+                
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'You are not the tenant of this office'
+                        },
+                        'id': id
+                    }
+                
+                office['tenant'] = ''
                 return {
                     'jsonrpc': '2.0',
                     'result': 'success',
