36ec6818f367b0c4cd64797f4ed7d86577247ac7
Редкачева Дарина <darinakaroche@mail.ru>
2025-12-06 00:39:31 +0700

    Заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 app = Flask(__name__)
 
@@ -21,6 +22,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 log404 = []
 @app.errorhandler(404)
@@ -153,6 +155,9 @@ def index():
             </ul>
             <ul>
                 <li><a href="/lab7/">Лабораторная работа 7</a></li>
+            </ul>
+            <ul>
+                <li><a href="/lab8/">Лабораторная работа 8</a></li>
             </ul>
         </nav>
         <footer>
